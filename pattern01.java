87514cc2c081c06ccb8262416e2ab7c5832134ea
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-04-18 10:14:42 +0530

    init commit

---
+++ pattern01.java
@@ -0,0 +1,10 @@
+class pattern01{
+    public static void trangle(){
+        System.out.println("trangle is running...");
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        trangle();
+        trangle();
+    }
+}
